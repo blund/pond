ffbb05188fdda2083e6c311d6580a4a6e82501d9
Børge Lundsaunet <boergelun@gmail.com>
2020-09-10 09:34:43 +0200

    Add timing measure

--- run.bat
+++ run.bat
@@ -1,7 +1,25 @@
 @echo off
 
+set startTime=%time%
+echo compile
 make compile
+
+echo Start compile: %startTime%
+echo End compile:   %time%
+
+
+set startTime=%time%
+echo.
+echo assemble
 gcc -m32 test.s
+
+echo Start assemble: %startTime%
+echo End assemble:   %time%
+
+echo execute
 a.exe
+
+echo.
+echo result:
 echo %ERRORLEVEL%
 del test.s
