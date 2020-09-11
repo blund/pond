e605be6d6967090acf6e1ebef9ebc1f22096a019
Børge Lundsaunet <boergelun@gmail.com>
2020-09-11 23:33:42 +0200

    update where out.s loaded from

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 echo
 /usr/bin/time -f'ghc compile time: %es' ghc -O2 Main.hs -o ./Build/pond -odir Build -hidir Build
 
-/usr/bin/time -f'compilation time time: %es' ./Build/pond Examples/test.c
+/usr/bin/time -f'compilation time: %es' ./Build/pond Examples/test.c
 
-/usr/bin/time -f'assembly time: %es' gcc out.s Foreign/print.c -o ./Build/a.out
+/usr/bin/time -f'assembly time: %es' gcc Build/out.s Foreign/print.c -o ./Build/a.out
 
 ./Build/a.out
