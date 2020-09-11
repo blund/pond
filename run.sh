c771ad6c1b58480e1c6499c7cb915e0a90ad54d7
Børge Lundsaunet <boergelun@gmail.com>
2020-09-11 23:19:01 +0200

    move output files to Build dir

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 
 echo
-/usr/bin/time -f'ghc compile time: %es' ghc -O2 Main.hs -o pond -odir Build -hidir Build
+/usr/bin/time -f'ghc compile time: %es' ghc -O2 Main.hs -o ./Build/pond -odir Build -hidir Build
 
-/usr/bin/time -f'compilation time time: %es' ./pond Examples/test.c
+/usr/bin/time -f'compilation time time: %es' ./Build/pond Examples/test.c
 
-/usr/bin/time -f'assembly time: %es' gcc out.s Foreign/print.c
+/usr/bin/time -f'assembly time: %es' gcc out.s Foreign/print.c -o ./Build/a.out
 
-./a.out
-
-rm a.out out.s pond
+./Build/a.out
