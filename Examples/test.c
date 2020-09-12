4dd237fb4c8eafa84d77f725b31890f4a22810a8
Børge Lundsaunet <boergelun@gmail.com>
2020-09-12 15:45:57 +0200

    change example extension to .pnd

--- Examples/test.c
+++
@@ -1,3 +0,0 @@
-int main () {
-    return -0b1001+0xff/(0b11+2);
-}
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 echo
 /usr/bin/time -f'ghc compile time: %es' ghc -O2 Main.hs -o ./Build/pond -odir Build -hidir Build
 
-/usr/bin/time -f'compilation time: %es' ./Build/pond Examples/test.c
+/usr/bin/time -f'compilation time: %es' ./Build/pond Examples/test.pnd
 
 /usr/bin/time -f'assembly time: %es' gcc Build/out.s Foreign/print.c -o ./Build/a.out
 
