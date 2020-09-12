e2f53f6503f6b664c47d6d9278f465daa1ef4a40
Børge Lundsaunet <boergelun@gmail.com>
2020-09-12 16:21:33 +0200

    stop execution when there is an error

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 
 echo
-/usr/bin/time -f'ghc compile time: %es' ghc -O2 Main.hs -o ./Build/pond -odir Build -hidir Build
-
-/usr/bin/time -f'compilation time: %es' ./Build/pond Examples/test.pnd
-
-/usr/bin/time -f'assembly time: %es' gcc Build/out.s Foreign/print.c -o ./Build/a.out
-
+/usr/bin/time -f'ghc compile time: %es' ghc -O2 Main.hs -o ./Build/pond -odir Build -hidir Build &&
+/usr/bin/time -f'compilation time: %es' ./Build/pond Examples/test.pnd &&
+/usr/bin/time -f'assembly time: %es' gcc Build/out.s Foreign/print.c -o ./Build/a.out &&
 ./Build/a.out
