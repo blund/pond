e4480d9c2ab25fd44138fef52505853db09319b2
Børge Lundsaunet <boergelun@gmail.com>
2020-09-12 16:20:58 +0200

    correct the path to example file

--- Main.hs
+++ Main.hs
@@ -8,29 +8,17 @@ import Pond.Backend
 import Pond.Frontend
 
 
-runCompile = do
-
-    handle <- openFile "Examples/test.c" ReadMode
-    file <- hGetContents handle
-
-    let ast = parseSource file
-
-    print ast
-
-    writeFile "out.s" $ compile ast
-
-    hClose handle
-
-
 main :: IO ()
 main = do
     args <- getArgs
+    case args of
+        [] -> print "Please pass the path to a source file\n"
+        [file] -> do
+            handle <- openFile (args !! 0) ReadMode
+            file <- hGetContents handle
 
-    handle <- openFile (args !! 0) ReadMode
-    file <- hGetContents handle
+            let ast = parseSource file
 
-    let ast = parseSource file
+            writeFile "Build/out.s" $ compile ast
 
-    writeFile "Build/out.s" $ compile ast
-
-    hClose handle
+            hClose handle
