d1568822408c87b42c4430b44731e5d71ed8d8d7
Børge Lundsaunet <boergelun@gmail.com>
2020-09-14 18:49:06 +0200

    add optional parameter to just run parsing

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 
-echo
-/usr/bin/time -f'ghc compile time: %es' ghc -O2 Main.hs -o ./Build/pond -odir Build -hidir Build &&
-/usr/bin/time -f'compilation time: %es' ./Build/pond Examples/test.pnd &&
-/usr/bin/time -f'assembly time: %es' gcc Build/out.s Foreign/print.c -o ./Build/a.out &&
-./Build/a.out
+case "$1" in
+    parse)
+        ghc -e parse Main.hs
+        ;;
+    *)
+        echo
+        /usr/bin/time -f'ghc compile time: %es' ghc -O2 Main.hs -o ./Build/pond -odir Build -hidir Build &&
+        /usr/bin/time -f'compilation time: %es' ./Build/pond Examples/test.pnd &&
+        /usr/bin/time -f'assembly time: %es' gcc Build/out.s Foreign/print.c -o ./Build/a.out &&
+        ./Build/a.out
+        ;;
+esac
