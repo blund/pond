ec9728da6080e6076e2370042996dd8b8b7b41be
Børge Lundsaunet <boergelun@gmail.com>
2020-09-18 16:55:02 +0200

    rename 'parse' to 'tree' as it is more fitting

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 
 case "$1" in
-    parse)
-        ghc -e parse Main.hs
+    tree)
+        ghc -e getTree Main.hs
         ;;
     *)
         echo
         /usr/bin/time -f'ghc compile time: %es' ghc -O2 Main.hs -o ./Build/pond -odir Build -hidir Build &&
         /usr/bin/time -f'compilation time: %es' ./Build/pond Examples/test.pnd &&
-        /usr/bin/time -f'assembly time: %es' gcc Build/out.s Foreign/print.c -o ./Build/a.out &&
+        /usr/bin/time -f'assembly time: %es' gcc -g Build/out.s Foreign/print.c -o ./Build/a.out &&
         ./Build/a.out
         ;;
 esac
