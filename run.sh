f6e9394990a23e6bf8fbf47772cd522af8eb24f4
Børge Lundsaunet <borgelu@stud.ntnu.no>
2020-10-03 22:32:15 +0200

    Remove timing, fix compilation folder structure on mac os
    Having the a compilation file called 'Pond' in the build directory
    alongside the exectuable 'pond' seemed to cause some issues. The build
    files were moved to a different folder, CompilationFiles

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ case "$1" in
         ;;
     *)
         echo
-        /usr/bin/time -f'ghc compile time: %es' ghc -O2 Main.hs -o ./Build/pond -odir Build -hidir Build &&
-        /usr/bin/time -f'compilation time: %es' ./Build/pond Examples/test.pnd &&
-        /usr/bin/time -f'assembly time: %es' gcc -g Build/out.s Foreign/print.c -o ./Build/a.out &&
+        mkdir -p ./Build/CompilationFiles
+        ghc Main.hs -o Build/pond -odir Build/CompilationFiles -hidir Build/CompilationFiles &&
+        ./Build/pond Examples/test.pnd &&
+        gcc -g Build/out.s Foreign/print.c -o ./Build/a.out &&
         ./Build/a.out
         echo $?
         ;;
